2747fda67d83dd9ed16ec8f362651b640035d22c
Edric Kyauk <edric.kyauk@gmail.com>
2015-10-02 14:07:39 -0700

    Removes old print lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,6 @@ class UsersController < ApplicationController
 
     def add_printer
         current_user.printer_id = params[:id]
-        puts 'pinrtayuntwfyutatnwfuynt'
-        puts current_user.printer_id
         current_user.save
         redirect_to '/printers'
     end
