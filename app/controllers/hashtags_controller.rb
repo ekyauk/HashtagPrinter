6a60e067735b2d0f91d98f1981bce983e474a96c
Edric Kyauk <edric.kyauk@gmail.com>
2015-10-02 14:14:14 -0700

    Adds one of the access token parameters back.

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -55,6 +55,7 @@ class HashtagsController < ApplicationController
             uri = URI("https://www.google.com/cloudprint/submit")
             fields = {
                 client_id: ENV['GOOGLE_CLIENT_ID'],
+                access_token: user.google_refresh_token,
                 printerid: user.printer_id,
                 title: 'Hashtag Printer',
                 ticket: {'version' => '1.0', 'print' => {}},
